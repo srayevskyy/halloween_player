d99067b1d1592cf9eeb2f4e9bcc91ee4ab0336dc
Your Name <you@example.com>
2016-10-16 08:56:11 +0000

    Async watch for sensor to trigger

--- halloween_player.go
+++ halloween_player.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver
 	"log"
 	"math/rand"
 	"os/exec"
@@ -16,13 +20,42 @@ func CheckError(message string, err error) {
 func main() {
 	resource_path := "/home/pi/Halloween/resources/"
 	resource_extension := "wav"
+	sensor_pin := 23
 
-	// init random number generator
-	rand.Seed(42)
-
+	// file resource init
 	files, err := filepath.Glob(resource_path + "/*" + resource_extension)
 	CheckError("Error wile scanning resource directory", err)
 	log.Printf("Found %d resource file(s) in %s", len(files), resource_path)
+
+	// random number generator init
+	rand.Seed(42)
+
+	// GPIO init
+	flag.Parse()
+	err = embd.InitGPIO()
+	CheckError("Error initlalizing GPIO", err)
+	defer embd.CloseGPIO()
+
+	btn, err := embd.NewDigitalPin(sensor_pin)
+	CheckError(fmt.Sprintf("Error wile accessing GPIO pin %d", sensor_pin), err)
+	defer btn.Close()
+
+	err = btn.SetDirection(embd.In)
+	CheckError(fmt.Sprintf("Error setting mode for GPIO pin %d", sensor_pin), err)
+
+	btn.ActiveLow(false)
+
+	quit := make(chan interface{})
+
+	log.Printf("Waiting for sensor to trigger...")
+
+	err = btn.Watch(embd.EdgeBoth,
+		func(btn embd.DigitalPin) {
+			quit <- btn
+		})
+	CheckError("Error wile watching for sensor to trigger", err)
+
+	log.Printf("Sensor %v has been triggered.\n", <-quit)
 
 	cmd := exec.Command("omxplayer", files[rand.Intn(len(files))])
 	err = cmd.Run()
