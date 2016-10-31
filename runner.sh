ab5957523970f23ff2c88c4e90e1903fa5a0abd7
Your Name <you@example.com>
2016-10-31 07:16:24 +0000

    Added runner

---
+++ runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/Go
+export PATH=$PATH:$GOROOT/bin
+
+LOGDATEFORMAT='+%Y-%m-%d %H:%M:%S %p'
+
+if ps axu | grep --silent "[h]alloween_player.go"; then
+	echo "$(date "${LOGDATEFORMAT}") INFO [${0}] Halloween palyer (halloween_player.go) is already running, exiting"
+	exit 0
+fi
+
+go run halloween_player.go
